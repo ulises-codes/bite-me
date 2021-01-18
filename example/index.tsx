dea64a27d6e13f60ab9eab92d99bbd2132065489
Ulises Himely <>
2021-01-18 16:46:26 -0500

    Created Offscreen component and extracted canvas rendering logic to web worker for more reliable performance, also just for fun

--- example/index.tsx
+++ example/index.tsx
@@ -2,7 +2,8 @@
 
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import SnakeGame from '../src/index';
+import SnakeGame from '../src/Snake';
+import OffScreenGame from '../src/Offscreen';
 import ImageURL from './assets/food.png';
 import AudioURL from './assets/echo.mp3';
 import DingURL from './assets/ding.mp3';
@@ -11,13 +12,17 @@ import GameOverURL from './assets/game-over.mp3';
 const App = () => {
   return (
     <div>
-      <SnakeGame
+      <OffScreenGame
         style={{ backgroundColor: '#24748F' }}
         foodImage={ImageURL}
         audioSrc={AudioURL}
         dingSrc={DingURL}
         gameOverSrc={GameOverURL}
-        text={{ color: '#F1DD6D' }}
+        text={{
+          color: '#2a2a2a',
+          subtitleColor: '#fafafa',
+          titleColor: '#F1DD6D',
+        }}
         snakeStyle={{
           color: ['#BF43A1', '#F26463', '#F1DD6D', '#2BACB3'],
         }}
