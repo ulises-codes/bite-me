26e8c304ad64a613200c97487fe8bc07b65e5e9c
Ulises Himely <47704656+ulises-codes@users.noreply.github.com>
2021-02-19 10:59:32 -0500

    Add button to mount & unmount game

--- example/index.tsx
+++ example/index.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import SnakeGame from '@ulises-codes/bite-me/offscreen';
+// import SnakeGame from '@ulises-codes/bite-me/offscreen';
 
-// import SnakeGame from '../dist/offscreen';
+import SnakeGame from '../dist/offscreen';
 
 import ImageURL from './assets/food.png';
 import AudioURL from './assets/echo.mp3';
@@ -11,26 +11,31 @@ import DingURL from './assets/ding.mp3';
 import GameOverURL from './assets/game-over.mp3';
 
 const App = () => {
+  const [play, setPlay] = React.useState(false);
+
   return (
     <div>
-      <SnakeGame
-        style={{ backgroundColor: '#24748F' }}
-        food={{ src: ImageURL }}
-        audioSrc={AudioURL}
-        dingSrc={DingURL}
-        gameOverSrc={GameOverURL}
-        text={{
-          color: '#2a2a2a',
-          subtitleColor: '#fafafa',
-          titleColor: '#F1DD6D',
-        }}
-        snakeStyle={{
-          color: ['#BF43A1', '#F26463', '#F1DD6D', '#2BACB3'],
-        }}
-        publicPath={
-          new URL('@ulises-codes/bite-me/dist/worker', import.meta.url)
-        }
-      />
+      <button onClick={() => setPlay(!play)}>{play ? 'Stop' : 'Play'}</button>
+      {play && (
+        <SnakeGame
+          style={{ backgroundColor: '#24748F' }}
+          food={{ src: ImageURL }}
+          audioSrc={AudioURL}
+          dingSrc={DingURL}
+          gameOverSrc={GameOverURL}
+          text={{
+            color: '#2a2a2a',
+            subtitleColor: '#fafafa',
+            titleColor: '#F1DD6D',
+          }}
+          snakeStyle={{
+            color: ['#BF43A1', '#F26463', '#F1DD6D', '#2BACB3'],
+          }}
+          publicPath={
+            new URL('@ulises-codes/bite-me/dist/worker', import.meta.url)
+          }
+        />
+      )}
     </div>
   );
 };
